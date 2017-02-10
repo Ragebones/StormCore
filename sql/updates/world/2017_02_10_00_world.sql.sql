3d6c44e3be689d36ed911eba6662fb36e9921ae5
Ragebones <adrianroll4ever@gmail.com>
2017-02-10 13:16:35 +0100

    Fixed possible crash with automatic db updater

--- sql/updates/world/2017_02_10_00_world.sql.sql
+++ sql/updates/world/2017_02_10_00_world.sql.sql
@@ -1,2 +1,4 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=6262;
+DELETE FROM `spell_script_names` WHERE `spell_id`=198590;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (198590, 'spell_warl_healthstone');
+
