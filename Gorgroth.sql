1c76ee1155fff95ae2f23e286214f410c281eb4b
Alaxel <wowficcion@gmail.com>
2017-02-11 16:15:36 -0400

    More Fixes

--- Gorgroth.sql
+++ Gorgroth.sql
@@ -1,14 +1,14 @@
 UPDATE `creature_template` SET `lootid`=110832, `minlevel`=110, `maxlevel`=110, `faction`=73 WHERE `entry`=110832;
-DELETE FROM `creature_loot_template` WHERE `entry`=110832;
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`)VALUES
-(110832, 121747, 0, 29, 0, 1, 1, 1, 'Hola'),
-(110832, 138839, 0, 21, 0, 1, 1, 1, 'Hola'),
-(110832, 138991, 0, 2, 1, 1, 1, 1, 'Hola'),
-(110832, 141572, 0, 17, 0, 1, 1, 1, 'Hola'),
-(110832, 132204, 0, 77, 0, 1, 1, 1, 'Hola'),
-(110832, 132199, 0, 19, 0, 1, 1, 1, 'Hola'),
-(110832, 124437, 0, 10, 0, 1, 1, 1, 'Hola'),
-(110832, 132200, 0, 6, 0, 1, 1, 1, 'Hola'); 
+DELETE FROM `creature_loot_template` WHERE ´entry´='110832';
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroudId`, `MinCount`, `MaxCount`, `Comment`)VALUES
+('110832', '121747', '0', '29', '0', '1', '1', '0', '1'),
+('110832', '138839', '0', '21', '0', '1', '1', '0', '1'),
+('110832', '138991', '0', '2', '1', '1', '1', '0', '1'),
+('110832', '141572', '0', '0.17', '0', '1', '1', '0', '1'),
+('110832', '132204', '0', '77', '0', '1', '1', '0', '1'),
+('110832', '132199', '0', '19', '0', '1', '1', '0', '1'),
+('110832', '124437', '0', '10', '0', '1', '1', '1', '3'),
+('110832', '132200', '0', '6', '0', '1', '1', '0', '1'); 
 
 -- Spawn 
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `PhaseId`, `PhaseGroup`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
